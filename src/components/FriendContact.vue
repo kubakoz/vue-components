e778ae634d0d70003ccb1c604fb4699f7b639322
kubakoz <jakubkozlowski@hotmail.com>
2021-03-22 22:46:39 -0400

    props and event handling

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,15 +1,20 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)': ''}}</h2>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavorite">{{ isFavorite ? 'Unset' : 'Set' }} Fave</button>
     <ul v-if="detailsAreVisible">
       <li>
         <strong>Phone:</strong>
-        {{ friend.phone }}
+        {{ phoneNumber }}
       </li>
       <li>
         <strong>Email:</strong>
-        {{ friend.email }}
+        {{ emailAddress }}
+      </li>
+      <li>
+        <strong>is Fave:</strong>
+        {{ isFavorite }}
       </li>
     </ul>
   </li>
@@ -17,20 +22,48 @@
 
 <script>
 export default {
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress'
+  // ], 
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {type: String, required: true},
+    phoneNumber: String, 
+    emailAddress: String,
+    isFavorite: {
+      type: Boolean, 
+      required: false, 
+      default: false
+    }
+  },
+  // emits: ['toggle-favorite'],  
+  emits: {
+    'toggle-favorite': function(id) {
+      if(id){
+        return true;
+      }else{
+        console.warn('id is missing');
+        return false
+      }
+    }
+  },
   data() {
     return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
+      detailsAreVisible: false
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      //this.isFavorite = !this.isFavorite;
+      this.$emit('toggle-favorite', this.id);
     }
   }
 };
